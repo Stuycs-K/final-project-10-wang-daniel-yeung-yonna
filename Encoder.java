b0129d6d0b1d177c73cac858ecdb1a6c060b07ee
Dwang40 <124080285+Dwang40@users.noreply.github.com>
2024-05-23 05:52:20 +0000

    Finished coding makeGrid, and made sure to test every method. This is just setup for me to do encoding later

--- Encoder.java
+++ Encoder.java
@@ -6,10 +6,9 @@ public class Encoder {
         return s.length() % 2 != 0;
     }
     
-    //Formats the String from "This is a sample text" to 
+    //Formats the String from "This is a sample text" to TH IS IS AS AM PL ET EX TX
     public static String format(String input) {
         String no = input.replace(" ", "").replace(".", "").toUpperCase();
-
         StringBuilder s = new StringBuilder();
 
         for (int i = 0; i < no.length(); i++) {
@@ -27,8 +26,40 @@ public class Encoder {
         return s.toString();
     }
 
+    //Makes the grid we need for the cipher
+    public static char[][] makeGrid(String key) {
+        char[][] grid = new char[5][5];
+        Set<Character> usedCh = new HashSet<>();
+        char current = 'a';
 
+        int index = 0;
+        for (int i = 0; i < key.length(); i++) {
+            char ch = key.charAt(i);
+            if (ch != 'j' && !usedCh.contains(ch)) {
+                usedCh.add(ch);
+                grid[index / 5][index % 5] = ch;
+                index++;
+            }
+        }
 
+        while (index < 25) {
+            if (current == 'j') {
+                current++;
+                continue;
+            }
+
+            if (!usedCh.contains(current)) {
+                grid[index / 5][index % 5] = current;
+                usedCh.add(current);
+                index++;
+            }
+            current++;
+        }
+
+        return grid;
+    }
+
+    //prints out the whole grid in a 5x5 block of text
     public static void printGrid(char[][] grid) {
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
@@ -38,9 +69,31 @@ public class Encoder {
         }
     }
 
+    //prints out a letter from the grid given the coordinates 
+    public static void printCoord(char[][] grid, int x, int y){
+        System.out.println(grid[x][y]);
+    }
+
     public static void main(String[] args) {
-        System.out.println(format("This is a sample text"));
-        //System.out.print(format("This is a long message to check whether or not this works properly."));
+        int x = 0;
+        int y = 0;
+
+        String s = "This is a long message to check whether or not this works properly.";
+
+        //Test the grid 
+        char[][] result = makeGrid("");
+        System.out.println("This is at (" + x + ", " + y +").");
+        printCoord(result, x, y);
+
+        System.out.println();
+        System.out.println("This is the full grid.");
+        printGrid(result);
+
+        System.out.println();
+
+        //Test the formatting
+        System.out.println("This is the string before the reformatting: " + s);
+        System.out.println(format(s));
     }
 }
 
