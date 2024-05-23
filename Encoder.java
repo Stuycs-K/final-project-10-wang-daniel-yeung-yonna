8e7071f0476958779d8fb2c90fe2b892b4aa6a07
Dwang40 <124080285+Dwang40@users.noreply.github.com>
2024-05-23 05:24:35 +0000

    made a format method perfect for the encoder for playfair cipher

--- Encoder.java
+++ Encoder.java
@@ -1,1 +1,33 @@
+import java.util.*;
+import java.io.*;
+public class Encoder {
+
+    public static boolean isOdd(String s){
+        return s.length() % 2 != 0;
+    }
+    
+    public static String format(String input) {
+        String no = input.replace(" ", "").replace(".", "").toUpperCase();
+
+        StringBuilder s = new StringBuilder();
+
+        for (int i = 0; i < no.length(); i++) {
+            s.append(no.charAt(i));
+
+            if ((i + 1) % 2 == 0 && i != no.length() - 1) {
+                s.append(' ');
+            }
+        }
+
+        if(isOdd(no.toString())){
+            s.append('X');
+        }
+
+        return s.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.print(format("This is a long message to check whether or not this works properly."));
+    }
+}
 
