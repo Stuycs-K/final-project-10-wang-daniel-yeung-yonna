bdd4809e0dc4299697fc712b2e5f6f8043239be6
Dwang40 <124080285+Dwang40@users.noreply.github.com>
2024-05-23 05:31:01 +0000

    accidently made a commit, was trying to add comments and make a 5x5 grid method

--- Encoder.java
+++ Encoder.java
@@ -6,6 +6,7 @@ public class Encoder {
         return s.length() % 2 != 0;
     }
     
+    //Formats the String from "This is a sample text" to 
     public static String format(String input) {
         String no = input.replace(" ", "").replace(".", "").toUpperCase();
 
@@ -26,8 +27,20 @@ public class Encoder {
         return s.toString();
     }
 
+
+
+    public static void printGrid(char[][] grid) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print(grid[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.print(format("This is a long message to check whether or not this works properly."));
+        System.out.println(format("This is a sample text"));
+        //System.out.print(format("This is a long message to check whether or not this works properly."));
     }
 }
 
