fd192af6e54645a1e90a66318a103051ca4af295
Daniel Wang <dwang40@stuy.edu>
2024-05-24 19:33:38 +0000

    formats working

--- Encoder.java
+++ Encoder.java
@@ -2,39 +2,39 @@ import java.util.*;
 import java.io.*;
 public class Encoder {
     public static String format(String s){
-        return s.replaceAll(" ", "").replaceAll(".", "").toUpperCase();
+        return s.replaceAll(" ", "").replaceAll("\\.", "").replaceAll(",", "").toUpperCase();   
     }
 
     public static String Lperm(String alpha, String a){
-        int pos = 0;
-        String newL = "";
+        int position = 0;
+        String changeAlpha = "";
         char letter = a.charAt(0);
         for(int i = 0; i < alpha.length() - 1; i++){
             if(alpha.charAt(i) == letter){
-                pos = i;
+                position = i;
             }
         }
-        int pos1 = pos + 1;
-        newL = alpha.substring(pos) + alpha.replaceAll(alpha.substring(pos), "");
-        String part = newL.substring(2, 14);
-        String perm = alpha.substring(pos, pos1) + part + newL.substring(1, 2) + newL.replaceAll(alpha.substring(pos, pos1), "").replaceAll(part, "").replaceAll(newL.substring(1,2), "");
-        return perm;
+        int position1 = position + 1;
+        changeAlpha = alpha.substring(position) + alpha.replaceAll(alpha.substring(position), "");
+        String part = changeAlpha.substring(2, 14);
+        String finalAlpha = alpha.substring(position, position1) + part + changeAlpha.substring(1, 2) + changeAlpha.replaceAll(alpha.substring(position, position1), "").replaceAll(part, "").replaceAll(changeAlpha.substring(1,2), "");
+        return finalAlpha;
     }
 
     public static String Rperm(String alpha, String a){
-        int pos = 0;
-        String newR = "";
+        int position = 0;
+        String changeAlpha = "";
         char letter = a.charAt(0);
         for(int i = 0; i < alpha.length() - 1; i++){
             if(alpha.charAt(i) == letter){
-                pos = i;
+                position = i;
             }
         }
-        int pos1 = pos + 1;
-        newR = alpha.substring(pos).replaceAll(alpha.substring(pos, pos1), "") + alpha.replaceAll(alpha.substring(pos), "") + alpha.substring(pos, pos1);
-        String part = newR.substring(3, 14);
-        String perm = newR.substring(0, 2) + part + newR.substring(2,3) + newR.replaceAll(newR.substring(0, 14), "");
-        return perm;
+        int position1 = position + 1;
+        changeAlpha = alpha.substring(position).replaceAll(alpha.substring(position, position1), "") + alpha.replaceAll(alpha.substring(position), "") + alpha.substring(position, position1);
+        String part = changeAlpha.substring(3, 14);
+        String finalAlpha = changeAlpha.substring(0, 2) + part + changeAlpha.substring(2,3) + changeAlpha.replaceAll(changeAlpha.substring(0, 14), "");
+        return finalAlpha;
     }
 
     public static String chaoEncoder(String alphaL, String alphaR, String plaintext){
@@ -51,6 +51,7 @@ public class Encoder {
 
         System.out.println(Lperm(LAlphabet, "C"));
         System.out.println(Rperm(RAlphabet, "A"));
+        System.out.println(plaintext);
         System.out.println(format(plaintext));
         
     }
