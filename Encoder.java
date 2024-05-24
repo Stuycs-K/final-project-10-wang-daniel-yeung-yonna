6b7488a792671d32d8ba5ddf3225fc1a642a1e75
Dwang40 <124080285+Dwang40@users.noreply.github.com>
2024-05-24 16:47:40 +0000

    finished the right alphabet permute method as well

--- Encoder.java
+++ Encoder.java
@@ -17,14 +17,27 @@ public class Encoder {
         return perm;
     }
 
-    public static void Rperm(String s){
-
+    public static String Rperm(String alpha, String a){
+        int pos = 0;
+        String newR = "";
+        char letter = a.charAt(0);
+        for(int i = 0; i < alpha.length() - 1; i++){
+            if(alpha.charAt(i) == letter){
+                pos = i;
+            }
+        }
+        int pos1 = pos + 1;
+        newR = alpha.substring(pos).replaceAll(alpha.substring(pos, pos1), "") + alpha.replaceAll(alpha.substring(pos), "") + alpha.substring(pos, pos1);
+        String part = newR.substring(3, 14);
+        String perm = newR.substring(0, 2) + part + newR.substring(2,3) + newR.replaceAll(newR.substring(0, 14), "");
+        return perm;
     }
 
     public static void main(String []args){
         String LAlphabet = "BFVGUHWJKNCPEDQRSTIXYLMOZA";
-        String RAlphabet = "HXUCZVAMDSLKPEFJRIGTWOBNYQ";
+        String RAlphabet = "CMOPRTUVJXAYZNBQDSEFGHLWIK";
         System.out.println(Lperm(LAlphabet, "C"));
+        System.out.println(Rperm(RAlphabet, "A"));
     }
 }
 
